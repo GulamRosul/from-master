441cfdd568457a1d6344f95b96a3d670124b8035
rosol111 <gulamrosul037@gmail.com>
2023-09-23 23:27:15 +0600

    Added by ruseable from using props and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
-import HookFrom from './Components/HookFrom/HookFrom'
+import ReusableFrom from './Components/ReusableFrom/ReusableFrom'
+// import HookFrom from './Components/HookFrom/HookFrom'
 // import RefFrom from './Components/RefFrom/RefFrom'
 // import StatefulFrom from './Components/StatefulFrom/StatefulFrom'
 // import SimpleFrom from './Components/SimpleFrom/SimpleFrom'
@@ -8,6 +9,13 @@ import HookFrom from './Components/HookFrom/HookFrom'
 function App() {
 
 
+  
+  const handleSingUpSubmit = data => {
+    console.log('sing up data', data);
+  }
+  const handleUpdateProfile = data => {
+  console.log('update profile', data);
+  }
   return (
     <>
    
@@ -15,7 +23,20 @@ function App() {
       {/* <SimpleFrom></SimpleFrom> */}
       {/* <StatefulFrom></StatefulFrom> */}
       {/* <RefFrom></RefFrom> */}
-      <HookFrom></HookFrom>
+      {/* <HookFrom></HookFrom> */}
+
+      <ReusableFrom formTitle={'sing up'} handleSubmit={handleSingUpSubmit}>
+        <div>
+          <h2>Sing Up</h2>
+          <p>Please sing up right now</p>
+        </div>
+      </ReusableFrom>
+      <ReusableFrom formTitle={'profile Update'} handleSubmit={handleUpdateProfile} SubmitBtnText='Update'>
+        <div>
+          <h2>Update Profile</h2>
+          <p>Always keep your profile updated</p>
+        </div>
+      </ReusableFrom>
     </>
   )
 }
