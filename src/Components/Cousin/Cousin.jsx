1bafc8d85023fba58f4a344400398a9ffec03bdb
rosol111 <gulamrosul037@gmail.com>
2023-09-24 04:37:33 +0600

    Added by Context API to avoid Prop drilling

--- src/Components/Cousin/Cousin.jsx
+++ src/Components/Cousin/Cousin.jsx
@@ -1,3 +1,4 @@
+import Friend from "../Friend/Friend";
 import Special from "../Special/Special";
 
 
@@ -7,7 +8,8 @@ const Cousin = ({name, asset}) => {
             <h2>Cousin</h2>
             <p>{name}</p>
             <section>
-                 { asset && <Special asset={asset}></Special>}
+                {asset && <Special asset={asset}></Special>}
+                {name==='Rubaiya' && <Friend></Friend>}
             </section>
         </div>
     );
--- src/Components/Grandpa/Grandpa.jsx
+++ src/Components/Grandpa/Grandpa.jsx
@@ -1,24 +1,29 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import "./Grandpa.css";
-const AssetContext = createContext('gold');
+export const AssetContext = createContext("gold");
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
-    const asset = 'diamond';
-    return (
-        <div className="grandpa ">
-            <h2>Grandpa</h2>
-            <AssetContext.Provider value="gold">
-            <section className="flex">
+    const [money, setMoney] = useState(1000);
+  const asset = "diamond";
+  return (
+    <div className="grandpa ">
+          <h2>Grandpa</h2>
+          <p>Net Money: {money }</p>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <AssetContext.Provider value="gold">
+          <section className="flex">
             <Dad asset={asset}></Dad>
             <Uncle asset={asset}></Uncle>
             <Aunty></Aunty>
-           </section>
-          </AssetContext.Provider>
-        </div>
-    );
+          </section>
+        </AssetContext.Provider>
+      </MoneyContext.Provider>
+    </div>
+  );
 };
 
 export default Grandpa;
