fdc3ccc8e561a9b92a8759304c1204a3df015b20
rosol111 <gulamrosul037@gmail.com>
2023-09-24 01:25:02 +0600

    Added by Create a list of components to create connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Grandpa from './Components/Grandpa/Grandpa'
 import ReusableFrom from './Components/ReusableFrom/ReusableFrom'
 // import HookFrom from './Components/HookFrom/HookFrom'
 // import RefFrom from './Components/RefFrom/RefFrom'
@@ -10,21 +11,22 @@ function App() {
 
 
   
-  const handleSingUpSubmit = data => {
-    console.log('sing up data', data);
-  }
-  const handleUpdateProfile = data => {
-  console.log('update profile', data);
-  }
+  // const handleSingUpSubmit = data => {
+  //   console.log('sing up data', data);
+  // }
+  // const handleUpdateProfile = data => {
+  // console.log('update profile', data);
+  // }
   return (
     <>
    
       <h1>FROM_MASTER</h1>
+      <Grandpa></Grandpa>
       {/* <SimpleFrom></SimpleFrom> */}
       {/* <StatefulFrom></StatefulFrom> */}
       {/* <RefFrom></RefFrom> */}
       {/* <HookFrom></HookFrom> */}
-
+{/* 
       <ReusableFrom formTitle={'sing up'} handleSubmit={handleSingUpSubmit}>
         <div>
           <h2>Sing Up</h2>
@@ -36,7 +38,7 @@ function App() {
           <h2>Update Profile</h2>
           <p>Always keep your profile updated</p>
         </div>
-      </ReusableFrom>
+      </ReusableFrom> */}
     </>
   )
 }
